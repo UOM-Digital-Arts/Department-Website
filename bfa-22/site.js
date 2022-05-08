18f29b06c3897eb3ce69ddadddcd4b54c5dfeed3
alessiacacciatore <alessia.cacciatore.19@um.edu.mt>
2022-05-08 23:52:31 +0200

    Student page responsiveness + fancy box
    javascript + stylesheet

--- bfa-22/site.js
+++ bfa-22/site.js
@@ -1,9 +1,12 @@
 //any image which is a direct child of a column within gallery-section 
 //will be wrapped with the lightbox wrapper
 if ($('.gallery-section').length > 0) {
+    //This is to change the cursor upon hover
     $('.gallery-section .col-4 > img').hover(function () {
         $(this).css("cursor", "zoom-in");
     });
+
+    //This is for the fancybox gallery view
     $('.gallery-section .col-4 > img').each(function () {
         var element = $(this);
         var imageSrc = element.attr('src');
@@ -11,15 +14,4 @@ if ($('.gallery-section').length > 0) {
 
     });
 
-}
-
-
-
-//Get the image from each student and place it as a background image for the text on mobile
-if ($('.piece_text').length > 0) {
-    $('.piece_text').each(function () {
-        var element = $(this);
-        var imageSrc = element.prev().find('img').attr('src');
-        element.prepend('<div class="mobile-bg-image" style="background-image: url(' + imageSrc + ');"></div>')
-    });
 }
