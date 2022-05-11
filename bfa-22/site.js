8a00b1d0927529d96c9a8618e97fba5c43a379af
alessiacacciatore <alessia.cacciatore.19@um.edu.mt>
2022-05-12 00:46:37 +0200

    sponsors

--- bfa-22/site.js
+++ bfa-22/site.js
@@ -6,8 +6,30 @@ if ($('.gallery-section').length > 0) {
         $(this).css("cursor", "zoom-in");
     });
 
+    $('.gallery-section .col-3 > img').hover(function () {
+        $(this).css("cursor", "zoom-in");
+    });
+
+    $('.gallery-section .col-6 > img').hover(function () {
+        $(this).css("cursor", "zoom-in");
+    });
+
     //This is for the fancybox gallery view
     $('.gallery-section .col-4 > img').each(function () {
+        var element = $(this);
+        var imageSrc = element.attr('src');
+        element.wrap('<a href="' + imageSrc + '" data-fancybox="gallery" ></a>');
+
+    });
+
+    $('.gallery-section .col-3 > img').each(function () {
+        var element = $(this);
+        var imageSrc = element.attr('src');
+        element.wrap('<a href="' + imageSrc + '" data-fancybox="gallery" ></a>');
+
+    });
+
+    $('.gallery-section .col-6 > img').each(function () {
         var element = $(this);
         var imageSrc = element.attr('src');
         element.wrap('<a href="' + imageSrc + '" data-fancybox="gallery" ></a>');
