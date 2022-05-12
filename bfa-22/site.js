61948b2bfc3be51618fda4d8264914068c2a648a
alessiacacciatore <alessia.cacciatore.19@um.edu.mt>
2022-05-12 02:25:38 +0200

    tweaks

--- bfa-22/site.js
+++ bfa-22/site.js
@@ -2,11 +2,11 @@
 //will be wrapped with the lightbox wrapper
 if ($('.gallery-section').length > 0) {
     //This is to change the cursor upon hover
-    $('.gallery-section .col-4 > img').hover(function () {
+    $('.gallery-section .col-3 > img').hover(function () {
         $(this).css("cursor", "zoom-in");
     });
 
-    $('.gallery-section .col-3 > img').hover(function () {
+    $('.gallery-section .col-4 > img').hover(function () {
         $(this).css("cursor", "zoom-in");
     });
 
@@ -14,14 +14,11 @@ if ($('.gallery-section').length > 0) {
         $(this).css("cursor", "zoom-in");
     });
 
-    //This is for the fancybox gallery view
-    $('.gallery-section .col-4 > img').each(function () {
-        var element = $(this);
-        var imageSrc = element.attr('src');
-        element.wrap('<a href="' + imageSrc + '" data-fancybox="gallery" ></a>');
-
+    $('.gallery-section .col-10 > img').hover(function () {
+        $(this).css("cursor", "zoom-in");
     });
 
+    //This is for the fancybox gallery view
     $('.gallery-section .col-3 > img').each(function () {
         var element = $(this);
         var imageSrc = element.attr('src');
@@ -29,7 +26,21 @@ if ($('.gallery-section').length > 0) {
 
     });
 
+    $('.gallery-section .col-4 > img').each(function () {
+        var element = $(this);
+        var imageSrc = element.attr('src');
+        element.wrap('<a href="' + imageSrc + '" data-fancybox="gallery" ></a>');
+
+    });
+
     $('.gallery-section .col-6 > img').each(function () {
+        var element = $(this);
+        var imageSrc = element.attr('src');
+        element.wrap('<a href="' + imageSrc + '" data-fancybox="gallery" ></a>');
+
+    });
+
+    $('.gallery-section .col-10 > img').each(function () {
         var element = $(this);
         var imageSrc = element.attr('src');
         element.wrap('<a href="' + imageSrc + '" data-fancybox="gallery" ></a>');
